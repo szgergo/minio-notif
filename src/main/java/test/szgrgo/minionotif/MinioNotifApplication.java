a849eeb61ebc59fe7e0f93e063cde6ed2c091f03
Gergő Szabó <szgrgo2@gmail.com>
2023-03-13 21:14:41 +0100

    Initial commit

---
+++ src/main/java/test/szgrgo/minionotif/MinioNotifApplication.java
@@ -0,0 +1,13 @@
+package test.szgrgo.minionotif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinioNotifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinioNotifApplication.class, args);
+	}
+
+}
---
+++ src/test/java/test/szgrgo/minionotif/MinioNotifApplicationTests.java
@@ -0,0 +1,13 @@
+package test.szgrgo.minionotif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinioNotifApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
